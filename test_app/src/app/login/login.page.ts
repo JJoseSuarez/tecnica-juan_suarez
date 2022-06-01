a5c68b2e69c2b92dc77c9c25dc49ca0ba24b1cf9
Juan José Suárez Ramírez <juan11857@gmail.com>
2022-06-01 11:58:19 +0200

    Feat. Add reactive form in login page with login form and modify login layout

--- test_app/src/app/login/login.page.ts
+++ test_app/src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  loginForm: FormGroup;
+
+  lockPassword: boolean = true;
+  typeTextPassword: string = 'password';
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
+    });
   }
 
+  showInputPassword(unlock: boolean) {
+   if (unlock && this.lockPassword === true) {
+     this.lockPassword = false;
+     this.typeTextPassword = 'text';
+   } else {
+     this.lockPassword = true;
+     this.typeTextPassword = 'password';
+   }
+  }
+  buttonAction(submitForm: string) {
+    if (this.loginForm.status !== 'INVALID') {
+      console.log(submitForm);
+    }
+  }
+  
 }
